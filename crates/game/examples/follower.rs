555cb9bbdc63f6714976b67c865f8a354fe52b07
Natalie Baker <technology@notvery.moe>
2024-03-18 17:11:39 +0800

    Add fps logging and change curve slightly

--- crates/game/examples/follower.rs
+++ crates/game/examples/follower.rs
@@ -1,12 +1,15 @@
 // Copyright 2024 Natalie Baker // AGPLv3 //
 
-use bevy::prelude::*;
+use bevy::{prelude::*, diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}};
 use game::{path_follower_system, LoopBehaviour, Path, PathFollower};
 use nvm_curve::{Bezier, Curve};
+#[cfg(debug_assertions)] // debug/dev builds only
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_plugins(LogDiagnosticsPlugin::default())
         .add_systems(PreUpdate, path_follower_system)
         .add_systems(Update, |q_followers: Query<&PathFollower>, mut gizmos: Gizmos| {
             for follower in &q_followers {
@@ -26,8 +29,8 @@ fn main() {
 fn create_path() -> Path {
     let mut path = Vec::<_>::default();
     for (i, curve) in [
-        Bezier::order_3(Vec2::ZERO,      Vec2::new( 135.0,   0.0), Vec2::new( 135.0, 200.0), Vec2::Y * 200.0),
-        Bezier::order_3(Vec2::Y * 200.0, Vec2::new(-135.0, 200.0), Vec2::new(-135.0,   0.0), Vec2::ZERO     ),
+        Bezier::order_3(Vec2::ZERO,      Vec2::new(-135.0, 100.0), Vec2::new( 135.0, 200.0), Vec2::Y * 300.0),
+        Bezier::order_3(Vec2::Y * 300.0, Vec2::new(-200.0, 200.0), Vec2::new(-200.0,   0.0), Vec2::ZERO     ),
     ].iter().enumerate() {
         curve.linearize(&mut path, 0.05, i == 0);
     }
